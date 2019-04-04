664ea91b59cd1fd06b488822aa064225529bdc4c
Jonathan Bartlett <jwb67@bath.ac.uk>
2019-04-04 13:21:19 +0100

    Adding with posterior norm imputation single variable.

--- R/mlmi.r
+++ R/mlmi.r
@@ -1,19 +1,37 @@
-normUniImp <- function(inputData, impFormula, M=5) {
+normUniImp <- function(inputData, impFormula, M=5, pd=FALSE) {
   #put some checks in to check there are no missing values in covariates of imp model
 
   #fit desired imputation model
   impMod <- lm(impFormula, data=inputData)
-  fittedVals <- predict(impMod, inputData)
 
   outcomeVar <- all.vars(impFormula)[1]
   print(outcomeVar)
 
   imps <- vector("list", M)
-  for (i in 1:M) {
-    imps[[i]] <- inputData
-    #impute
-    imps[[i]][is.na(inputData[,outcomeVar]),outcomeVar] <- fittedVals[is.na(inputData[,outcomeVar])] +
-      rnorm(sum(is.na(inputData[,outcomeVar])), mean=0, sd=sigma(impMod))
+
+  if (pd==FALSE) {
+    fittedVals <- predict(impMod, inputData)
+    print(fittedVals)
+    for (i in 1:M) {
+      imps[[i]] <- inputData
+      #impute
+      imps[[i]][is.na(inputData[,outcomeVar]),outcomeVar] <- fittedVals[is.na(inputData[,outcomeVar])] +
+        rnorm(sum(is.na(inputData[,outcomeVar])), mean=0, sd=sigma(impMod))
+    }
+  } else {
+    beta <- impMod$coef
+    sigmasq <- summary(impMod)$sigma^2
+    varcov <- vcov(impMod)
+    for (i in 1:M) {
+      imps[[i]] <- inputData
+      outcomeModResVar <- (sigmasq*impMod$df) / rchisq(1,impMod$df)
+      covariance <- (outcomeModResVar/sigmasq)*varcov
+      outcomeModBeta <- beta + MASS::mvrnorm(1, mu=rep(0,ncol(covariance)), Sigma=covariance)
+      impMod$coefficients <- outcomeModBeta
+      fittedVals <- predict(impMod, inputData)
+      imps[[i]][is.na(inputData[,outcomeVar]),outcomeVar] <- fittedVals[is.na(inputData[,outcomeVar])] +
+        rnorm(sum(is.na(inputData[,outcomeVar])), mean=0, sd=outcomeModResVar^0.5)
+    }
   }
 
   imps
