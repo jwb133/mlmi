f4b5a9315e6c585cb081fbcc6e559c6184cf15aa
Jonathan Bartlett <j.w.bartlett@bath.ac.uk>
2019-05-29 17:42:04 -0400

    Adding explanatory note to new version of h()

--- R/common.r
+++ R/common.r
@@ -1,6 +1,8 @@
 #shrinkage functions
 
 #shrinkage function h(,)
+#we calculate log(h,) first, to avoid numerical overflow problems. This avoids calculating
+#the two incomplete gamma function values directly, which can be very large
 h <- function(gamma,nu) {
   exp(log(nu/2) + log(gamma) + gsl::lngamma((nu-2)/2) + log(gsl::gamma_inc_Q((nu-2)/2, nu*gamma/2)) -
     (gsl::lngamma(nu/2) + log(gsl::gamma_inc_Q(nu/2, nu*gamma/2))))
